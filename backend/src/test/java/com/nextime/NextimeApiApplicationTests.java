5a1043f86d5ba75bb3c4fc89e2edb4debecdbc74
jungwoow <yeriel.390@gmail.com>
2026-08-06 23:21:10 +0900

    chore: 백엔드 프로젝트 생성

---
+++ backend/src/test/java/com/nextime/NextimeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextimeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
