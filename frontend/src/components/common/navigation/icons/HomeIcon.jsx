a5a84704db5a7e986fd1a5cb05a7cabc5cef12a9
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-08-12 04:43:11 +0900

    fix: 메뉴별 아이콘 svg 컴포넌트 수정

--- frontend/src/components/common/navigation/icons/HomeIcon.jsx
+++ frontend/src/components/common/navigation/icons/HomeIcon.jsx
@@ -1,6 +1,4 @@
-function HomeIcon({ $active }) {
-  const currentColor = $active ? theme.colors.primary.default : "#68686D";
-
+function HomeIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -12,16 +10,16 @@ function HomeIcon({ $active }) {
       <path
         d="M2.74969 8.24903L10.9989 1.83301L19.248 8.24903V18.3314C19.248 18.8175 19.0549 19.2838 18.7111 19.6276C18.3673 19.9714 17.9011 20.1645 17.4149 20.1645H4.58284C4.09666 20.1645 3.63039 19.9714 3.28661 19.6276C2.94283 19.2838 2.74969 18.8175 2.74969 18.3314V8.24903Z"
         stroke="currentColor"
-        stroke-width="1.64983"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.64983"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
       <path
         d="M8.24915 20.1648V10.999H13.7486V20.1648"
         stroke="currentColor"
-        stroke-width="1.64983"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        strokeWidth="1.64983"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
--- frontend/src/components/common/navigation/icons/PatternIcon.jsx
+++ frontend/src/components/common/navigation/icons/PatternIcon.jsx
@@ -1,6 +1,4 @@
-function PatternIcon($active) {
-  const currentColor = $active ? theme.colors.primary.default : "#68686D";
-
+function PatternIcon() {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
@@ -11,10 +9,10 @@ function PatternIcon($active) {
     >
       <path
         d="M20.1646 10.9989H16.4983L13.7486 19.2481L8.24916 2.74976L5.49943 10.9989H1.83313"
-        stroke="#currentColor"
-        stroke-width="1.64983"
-        stroke-linecap="round"
-        stroke-linejoin="round"
+        stroke="currentColor"
+        strokeWidth="1.64983"
+        strokeLinecap="round"
+        strokeLinejoin="round"
       />
     </svg>
   );
