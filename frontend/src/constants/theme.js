aefeeeec7e5425ec1dcb578551920802e44f7565
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-08-12 00:35:52 +0900

    settings: 앱 레이아웃 설정

--- frontend/src/constants/theme.js
+++ frontend/src/constants/theme.js
@@ -1,43 +1,43 @@
 export const lightTheme = {
-    colors: {
-        bg: {
-            default: '#F7F7FA',
-            surface: '#FFFFFF',
-        },
-
-        text: {
-            primary: '#191B24',
-            secondary: '#686D79',
-        },
-
-        border: {
-            default: '#E5E6EC',
-        },
-        
-        primary: {
-            default: '#00FF90',
-        },
+  colors: {
+    bg: {
+      default: "#F7F7FA",
+      surface: "#FFFFFF",
     },
-},
+
+    text: {
+      primary: "#191B24",
+      secondary: "#686D79",
+    },
+
+    border: {
+      default: "#E5E6EC",
+    },
+
+    primary: {
+      default: "#00FF90",
+    },
+  },
+};
 
 export const nextTimeTheme = {
-    colors: {
-        bg: {
-            default: '#252843',
-            surface: '#303451',
-        },
-
-        text: {
-            primary: '#F7F7FA',
-            secondary: '#D9D9D9',
-        },
-
-        border: {
-            default: '#3D4261',
-        },
-
-        primary: {
-            default: '#3DDC97',
-        },
+  colors: {
+    bg: {
+      default: "#252843",
+      surface: "#303451",
     },
+
+    text: {
+      primary: "#F7F7FA",
+      secondary: "#D9D9D9",
+    },
+
+    border: {
+      default: "#3D4261",
+    },
+
+    primary: {
+      default: "#3DDC97",
+    },
+  },
 };
