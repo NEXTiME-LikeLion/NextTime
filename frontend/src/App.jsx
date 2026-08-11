9a58a09fa2df4bf91e048782df16935103bafd1d
jiwoo choi <chlwldn418@naver.com>
2026-08-12 00:48:28 +0900

    feat: 스플래시  화면 구현 및 로그인/회원가입 파일 생성

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,14 @@
+// App.jsx (테스트용 임시 버전)
 import "./App.css";
-import { RouterProvider } from "react-router-dom";
-import router from "./router";
+import { BrowserRouter } from "react-router-dom";
+import Splash from "./pages/Splash/Splash";
 
 function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <BrowserRouter>
+      <Splash />
+    </BrowserRouter>
+  );
 }
 
 export default App;
---
+++ frontend/src/pages/Splash/Splash.styles.js
@@ -0,0 +1,26 @@
+// src/components/Splash/SplashScreen.styles.js
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background: #3DDC97;
+`;
+
+export const MascotWrapper = styled.div`
+  margin-bottom: 32px;
+`;
+
+export const MascotImage = styled.img`
+  width: 160px;
+  height: auto;
+`;
+
+export const LogoImage = styled.img`
+  width: 280px;
+  height: auto;
+`;
