8db583403c829bb82f620bcb254ba35bf7d108d7
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-08-13 20:57:23 +0900

    feat: 홈 화면 상단 Header 내부 컴포넌트 작성

--- frontend/src/components/home/HomeContent.jsx
+++ frontend/src/components/home/HomeContent.jsx
@@ -1,5 +1,11 @@
-function HomeContent(props) {
-  return <div></div>;
+import styled from "styled-components";
+
+function HomeContent() {
+  return <Container></Container>;
 }
 
 export default HomeContent;
+
+const Container = styled.div`
+  padding-block: 1.75rem;
+`;
--- frontend/src/components/home/HomeHeader.jsx
+++ frontend/src/components/home/HomeHeader.jsx
@@ -1,5 +1,30 @@
-function HomeHeader(props) {
-  return <div></div>;
+import styled from "styled-components";
+
+function HomeHeader() {
+  return (
+    <>
+      <TabName>홈</TabName>
+      <Container>
+        <Left></Left>
+        <Right></Right>
+      </Container>
+    </>
+  );
 }
 
 export default HomeHeader;
+
+const TabName = styled.p`
+  margin-bottom: 0.75rem;
+
+  color: #fff;
+  font-size: 1.25rem;
+  font-weight: 800;
+  line-height: 140%; /* 1.75rem */
+`;
+
+const Container = styled.div`
+  display: flex;
+  justify-content: space-between;
+  /* gap: 1.37rem; */
+`;
