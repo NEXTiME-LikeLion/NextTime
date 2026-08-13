885940abdbcdfbc64e652548351b51346bbe1191
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-08-13 22:22:54 +0900

    refactor: theme.js 수정

--- frontend/src/constants/theme.js
+++ frontend/src/constants/theme.js
@@ -1,52 +1,26 @@
-export const brand = {
+export const theme = {
+  colors: {
+    bg0: "#FEFEFE",
+    bg1: "#252843",
+    bg_black: "#0E1022",
+    primary: "#00D579",
+    light_gray: "#B2B2B2",
+    gray: "#68686D",
+    white: "#FFFFFF",
+  },
+
+  brand: {
     auth: {
-        primary: '#00D579',
-        textMuted: '#B2B2B2',
+      primary: "#00D579",
+      textMuted: "#B2B2B2",
     },
     toast: {
-        bg: '#252843',
+      bg: "#252843",
     },
     splash: {
-        bg: '#73D182',
-        mascotBody: '#F6F6EA',
-        belly: '#9EF09E',
+      bg: "#73D182",
+      mascotBody: "#F6F6EA",
+      belly: "#9EF09E",
     },
-};
-
-export const lightTheme = {
-    colors: {
-        bg: {
-            default: '#F7F7FA',
-            surface: '#FFFFFF',
-        },
-        text: {
-            primary: '#191B24',
-            secondary: '#686D79',
-        },
-        border: {
-            default: '#E5E6EC',
-        },
-        primary: {
-            default: '#00D579',
-        },
-    },
-};
-
-export const nextTimeTheme = {
-    colors: {
-        bg: {
-            default: '#252843',
-            surface: '#303451',
-        },
-        text: {
-            primary: '#F7F7FA',
-            secondary: '#D9D9D9',
-        },
-        border: {
-            default: '#3D4261',
-        },
-        primary: {
-            default: '#00D579',
-        },
-    },
+  },
 };
