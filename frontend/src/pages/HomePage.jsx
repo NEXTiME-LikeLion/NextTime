9cb2257aca62b6ce670a81f65a053654efacad28
jumeok <sarah1020zzz@sookmyung.ac.kr>
2026-08-18 13:46:38 +0900

    feat: 홈 화면 조회 API 호출

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,39 @@
 import TabMainLayout from "../layouts/TabMainLayout";
 import HomeHeader from "../components/home/HomeHeader";
 import HomeContent from "../components/home/HomeContent";
+import { useEffect, useState } from "react";
+import { getHome } from "../api/home";
 
 function HomePage() {
-  return <TabMainLayout header={<HomeHeader />} content={<HomeContent />} />;
+  const [homeData, setHomeData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    getHome()
+    .then((res) => {
+      setHomeData(res.data);
+    })
+    .catch((err) => {
+      console.error(err);
+      setHomeData(null);
+    })
+    .finally(()=> 
+      setIsLoading(false)
+    );
+  }, []);
+
+  if (isLoading) {
+    return <div>로딩 중입니다.</div>;
+  }
+  if (!homeData) {
+    return <div>에러가 발생했어요. 다시 시도해주세요.</div>;
+  }
+
+  return <TabMainLayout 
+    header={<HomeHeader nextMe={homeData.nextMe} />} 
+    content={<HomeContent todaySummary={homeData.todaySummary} />} 
+  />;
 }
+
 
 export default HomePage;
